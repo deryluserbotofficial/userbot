b3754db0c003d0e477d574a9f5efb70d9bab5bb8
deryluserbotofficial <86948955+deryluserbotofficial@users.noreply.github.com>
2021-07-05 16:37:25 +0800

    Create __init__.py

---
+++ derylbot/__init__.py
@@ -0,0 +1,70 @@
+import os
+import sys
+import time
+from distutils.util import strtobool as sb
+from logging import DEBUG, INFO, basicConfig, getLogger
+
+import heroku3
+from dotenv import load_dotenv
+from requests import get
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+from .Config import Config
+
+StartTime = time.time()
+catversion = "1.0.0"
+
+if Config.STRING_SESSION:
+    session_name = str(Config.STRING_SESSION)
+    if session_name.endswith("="):
+        bot = TelegramClient(
+            StringSession(session_name), Config.APP_ID, Config.API_HASH
+        )
+    else:
+        bot = TelegramClient(
+            "TG_BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
+        ).start(bot_token=Config.STRING_SESSION)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
+
+
+DER_ID = ["1614229158", "1380685409"]
+
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=INFO
+    )
+LOGS = getLogger(__name__)
+
+try:
+    if Config.API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        NAMA_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        NAMA_APP = None
+except Exception:
+    NAMA_APP = None
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
